77756718da2dbda318e41d778daeb63f5730ccc9
zhouyu-zi <73690612+zhouyu-zi@users.noreply.github.com>
2021-10-17 17:16:18 +0800

    pull commit

--- src/com/zy/git/GitTest.java
+++ src/com/zy/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("git hello!-4");
         System.out.println("master test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
