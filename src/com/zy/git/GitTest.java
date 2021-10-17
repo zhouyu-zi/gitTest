b3adc5fd95caa7312c20b0ec175ec466400f021c
zhouyu <2282756219@qq.com>
2021-10-17 17:09:07 +0800

    push commit

--- src/com/zy/git/GitTest.java
+++ src/com/zy/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("git hello!-3");
         System.out.println("git hello!-4");
         System.out.println("master test");
+        System.out.println("push test");
     }
 }
